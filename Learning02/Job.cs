7a5d9095cfe1cbc1fe6b1a1bd6298f8ee125ad15
paulomath28 <67445880+paulomath28@users.noreply.github.com>
2024-03-06 23:20:13 -0300

    Add files via upload

--- Learning02/Job.cs
+++ Learning02/Job.cs
@@ -1,1 +1,34 @@
+// Resume.cs
+using System;
+using System.Collections.Generic;
 
+public class Resume
+{
+    // Member variables
+    private string _personName;
+    private List<Job> _jobs;
+
+    // Constructor
+    public Resume(string personName)
+    {
+        _personName = personName;
+        _jobs = new List<Job>();
+    }
+
+    // Method to add a job to the resume
+    public void AddJob(Job job)
+    {
+        _jobs.Add(job);
+    }
+
+    // Method to display resume details
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_personName}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
